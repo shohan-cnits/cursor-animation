e48da60e573ed9dc19b388c8679ae3d8f2a5f247
shohan-cnits <shohan.cnits@gmail.com>
2024-02-15 10:40:14 +0600

    cursor animation done

---
+++ cursor.js
@@ -0,0 +1,7 @@
+cursor = document.querySelector(".cursor");
+cursor2 = document.querySelector(".cursor2");
+
+document.addEventListener("mousemove", function(e){
+cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX +
+ "px; top: " + e.clientY + "px;";
+});
